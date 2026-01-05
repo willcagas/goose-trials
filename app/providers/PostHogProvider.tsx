fde507ab7a15635c324e4a3b6e1e91fc75df3ad5
willcagas <williamgabriel.cagas@gmail.com>
2026-01-05 16:05:04 -0500

    fix: tracking for posthog block

--- app/providers/PostHogProvider.tsx
+++ app/providers/PostHogProvider.tsx
@@ -7,7 +7,7 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: '/ingest',
+        api_host: '/api/v2',
         ui_host: 'https://us.i.posthog.com',
         person_profiles: 'identified_only',
         capture_pageview: false,
