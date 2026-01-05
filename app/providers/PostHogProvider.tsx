1be88f9c28642aa7a2e94ece49162080b88607f9
willcagas <williamgabriel.cagas@gmail.com>
2026-01-05 15:50:09 -0500

    feat: add posthog proxy

--- app/providers/PostHogProvider.tsx
+++ app/providers/PostHogProvider.tsx
@@ -7,7 +7,8 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+        api_host: '/ingest',
+        ui_host: 'https://us.i.posthog.com',
         person_profiles: 'identified_only',
         capture_pageview: false,
         capture_pageleave: true
